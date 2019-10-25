26cf999d70ff27f95de5d0aa3e0e56d6b922cb50
grantkee <grant.d.kee@gmail.com>
2019-10-24 21:58:10 -0500

    console logged to get the name of the beers, but I still cant create an object using react

--- src/App.js
+++ src/App.js
@@ -18,13 +18,16 @@ class App extends Component {
   fetchData() {
     fetch ('https://api.punkapi.com/v2/beers')
     .then(response => response.json())
-    .then(parsedJSON => parsedJSON.results.map(beer => ({
-      name: `${beer.name}`,
-      tagline: `${beer.tagline}`,
-      abv: `${beer.abv}`,
-      ibu: `${beer.ibu}`,
-      mash_temp_value_c: `${beer.method.mash_temp.temp.value}`
-    })))
+    .then(parsedJSON => console.log(parsedJSON.map(beer => (beer.name))))
+    // .then(parsedJSON => parsedJSON.map(beer => (
+    //   {
+    //   name: `${beer.name}`,
+    //   tagline: `${beer.tagline}`,
+    //   abv: `${beer.abv}`,
+    //   ibu: `${beer.ibu}`,
+    //   mash_temp_value_c: `${beer.method.mash_temp.temp.value}`
+    // }
+    // )))
     .catch(error => console.log("parsing failed", error))
   }
 
