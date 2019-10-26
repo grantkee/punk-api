7c6fd531efa12898e20a37408b15e2b74c27b991
grantkee <grant.d.kee@gmail.com>
2019-10-26 09:55:30 -0500

    console logged out some stuff to get the array into an object

--- src/App.js
+++ src/App.js
@@ -19,15 +19,15 @@ class App extends Component {
     fetch ('https://api.punkapi.com/v2/beers')
     .then(response => response.json())
     .then(parsedJSON => console.log(parsedJSON.map(beer => (beer.name))))
-    // .then(parsedJSON => parsedJSON.map(beer => (
-    //   {
-    //   name: `${beer.name}`,
-    //   tagline: `${beer.tagline}`,
-    //   abv: `${beer.abv}`,
-    //   ibu: `${beer.ibu}`,
-    //   mash_temp_value_c: `${beer.method.mash_temp.temp.value}`
-    // }
-    // )))
+    .then(parsedJSON => parsedJSON.map(beer => (
+      {
+      name: `${beer.name}`,
+      tagline: `${beer.tagline}`,
+      abv: `${beer.abv}`,
+      ibu: `${beer.ibu}`,
+      mash_temp_value_c: `${beer.method.mash_temp.temp.value}`
+    }
+    )))
     .catch(error => console.log("parsing failed", error))
   }
 
